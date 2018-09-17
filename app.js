d5c50aeb7b5ba7115e965d95cd7d465eb5f21567
Dashiell <huebnerd08@gmail.com>
2018-09-16 20:10:36 -0700

    q3 code

--- app.js
+++ app.js
@@ -24,17 +24,8 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-  var newProduct = a * b;
-  var summary = 'The product of ' + a + ' and ' + b + ' is ' + newProduct + '.';
-  return[newProduct, summary];
-}
-
-var prodArray = multiply(5, 9);
-console.log(prodArray);
-
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -51,11 +42,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var sumElement = sum(a, b)[0];
+console.log(sumElement);
+// var productElement = multiply(a, b)[0], c)[0];
+// var sumElementArray = a + ' and ' + b + ' and ' c + ' sum to ' sumElement + '.';
+// var poductElementArray = 'The product of ' + a + ' and ' + ' and ' + c ' is ' productElement + '.';
+
+// return [sumElement,productElement,sumElementArray,productElementArray];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
